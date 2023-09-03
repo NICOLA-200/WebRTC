e395aa14b3ba98a76d2f65cd2e46bb535c4d4b65
NICOLA-200 <fatepepe66@gmail.com>
2023-09-03 09:51:30 +0200

    finished script

--- public/script.js
+++ public/script.js
@@ -25,3 +25,32 @@ navigator.mediaDevices.getUserMedia({
     connectToNewUser(userId, stream)
   })
 })
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
